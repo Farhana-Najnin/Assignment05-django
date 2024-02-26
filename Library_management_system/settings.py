1971d5915264e6a6a75545bdbb3fc0a58a5a33c3
Farhana Najnin Mahi <farhananajninmahi0845@gmail.com>
2024-02-27 01:51:24 +0600

    changes done

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY=env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ['https://library-system-f02c.onrender.com','https://*.127.0.0.1']
 
 
